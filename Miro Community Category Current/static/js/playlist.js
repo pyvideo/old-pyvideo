8b60b216286d3e77ebee1e3e4f5ba667796c9105
Will Kahn-Greene <willg@bluesock.org>
2010-09-01 18:07:15 -0400

    Updating theme for Miro Community 1.1.1.

---
+++ Miro Community Category Current/static/js/playlist.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#id_playlist").append('<option value="new">New Playlist...</option>');
+    $("#playlists + form + form").hide()
+    $("#playlists .playlist_thumbs").hide()
+    $("#playlists .playlist_title > a").each(function(){
+        flip = 0;
+        $(this).click(function() {
+            parent = $(this).parent();
+            thumbs = parent.next();
+            if (++flip % 2) {
+                parent.addClass('open')
+                thumbs.show(400);
+            } else {
+                parent.removeClass('open')
+                thumbs.hide();
+            }
+            return false;
+        });
+    });
+    $("#id_playlist").parents('form').submit(function() {
+        that = $(this);
+        field = that.find("#id_playlist");
+        if (!field.val()) {
+            return false;
+        }
+        if (field.val() != 'new') {
+            // keep going!
+            return true;
+        }
+        if (!(name = prompt("Enter the name for the new playlist:"))) {
+            return false;
+        }
+        if (name == "") {
+            return false;
+        }
+
+        new_playlist_form = that.next();
+        new_playlist_form.find('input[name=name]').val(name);
+        new_playlist_form.submit()
+        return false;
+    });
+});
