60c97b6720743bd8ca3dd2b2afa34f55be868818
Will Kahn-Greene <willg@bluesock.org>
2012-03-03 20:59:29 -0500

    Add new pyvideo stuff

---
+++ reset_everything.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PYTHON=./venv/bin/python
+FIXTURES=./fixtures
+
+$PYTHON ./richard/manage.py reset videos sitenews
+$PYTHON ./richard/manage.py syncdb
+
+$PYTHON ./richard/manage.py loaddata \
+    $FIXTURES/tags.json \
+    $FIXTURES/speakers.json \
+    $FIXTURES/categories.json \
+    $FIXTURES/bostonpy.json \
+    $FIXTURES/chipy.json \
+    $FIXTURES/pyatl.json \
+    $FIXTURES/djangocon-2009.json \
+    $FIXTURES/djangocon-2010.json \
+    $FIXTURES/djangocon-2011.json \
+    $FIXTURES/kiwi-pycon-2009.json \
+    $FIXTURES/pycon-2009.json \
+    $FIXTURES/pycon-2010.json \
+    $FIXTURES/pycon-2011.json \
+    $FIXTURES/pycon-au-2010.json \
+    $FIXTURES/pygotham-2011.json \
+    $FIXTURES/pyohio-2010.json \
+    $FIXTURES/pyohio-2011.json \
+    $FIXTURES/pytexas-2011.json
+
+#    pycon-au-2011.json \
