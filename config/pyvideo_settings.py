7e5bf07679f7e627630acec758d65f6115b1610a
Will Kahn-Greene <willg@bluesock.org>
2012-04-11 23:59:47 +0000

    Add staticbase to STATICfiLES
    This way the favicon gets copied over with collectstatic and I can stop doing
    it by hand.

--- config/pyvideo_settings.py
+++ config/pyvideo_settings.py
@@ -99,6 +99,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'staticbase'),
     os.path.join(ROOT, 'static'),
 )
 
