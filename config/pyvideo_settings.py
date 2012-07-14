a25395dac88a07f6448235fa7aba8e73c4d5863b
Will Kahn-Greene <willg@bluesock.org>
2012-07-14 16:36:21 -0400

    Base config on richard config
    This changes the pyvideo_settings.py so it pulls everything from
    richard settings and then overrides the important things.

--- config/pyvideo_settings.py
+++ config/pyvideo_settings.py
@@ -1,3 +1,5 @@
+from richard.settings import *
+
 import mysecrets
 
 import sys
@@ -8,7 +10,6 @@ SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 # root is this directory
 ROOT = os.path.join(SITE_ROOT, 'richard', 'richard')
-
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -48,37 +49,6 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = False
-
-# Fixture directory for easy bulk-loading
-FIXTURE_DIRS = (
-    os.path.join(SITE_ROOT, 'fixtures'),
-    )
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
@@ -107,123 +77,13 @@ STATICFILES_DIRS = (
     os.path.join(ROOT, 'static'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = mysecrets.SECRET_KEY
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'jingo.Loader',
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # This should probably be last. It catches 404 errors, then checks
-    # to see if we should be redirecting the url.
-    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
-)
-
-ROOT_URLCONF = 'richard.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'richard.wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(SITE_ROOT, 'templates'),
     os.path.join(ROOT, 'templates'),
 )
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-
-    'richard.context_processors.base',
-)
-
-JINGO_EXCLUDE_APPS = (
-    'debug_toolbar',
-    'django.contrib.admin',
-    'admin',
-)
-
-JINJA_CONFIG = {
-    'extensions': ['jinja2.ext.with_'],
-}
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.redirects',
-    'django.contrib.sitemaps',
-    'haystack',
-    'south',
-    'tastypie',
-
-    'richard.videos',
-    'richard.sitenews',
-    'richard.pages',
-    'richard.suggestions',
-)
-
-try:
-    # Add django_nose for testing but only if nose is installed.
-    import nose
-    INSTALLED_APPS = tuple(list(INSTALLED_APPS) + ['django_nose'])
-except ImportError:
-    pass
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
 
 EMAIL_SUBJECT_PREFIX = '[Django pyvideo]'
 
