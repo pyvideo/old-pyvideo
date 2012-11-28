eeabc680ef63935c6e29f95386769d94d0d1c058
Will Kahn-Greene <willg@bluesock.org>
2012-11-27 22:47:17 -0500

    Add manage.sh script, update templates
    This updates the templates after the bootstrap update.

--- manage.sh
+++ manage.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export PYTHONPATH=./config:./richard:$PYTHONPATH
-export DJANGO_SETTINGS_MODULE=pyvideo_settings
-
-./venv/bin/python richard/manage.py $@
+PYTHONPATH=./config DJANGO_SETTINGS_MODULE=pyvideo_settings richard/manage.py $@
